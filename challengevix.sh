6b5da9718a9e31b761fd6e424221094eb716e02f
syahbananur <146501229+syahbananur@users.noreply.github.com>
2023-09-30 11:26:59 +0800

    Delete challengevix.sh

--- challengevix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-path=hdfs/data/data1
-name_of_directory=data1
-if [ -d $dir ]; then
- echo "There is ${name_of_directory} Directory Exists!"
-else
- echo "${name_of_directory Directory} Not Exists!"
- mkdir $sdir
-fi
